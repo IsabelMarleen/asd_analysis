7acfad23be83a1232d6b8a027b1c9dfbab6eb38a
IsabelMarleen <isabel-marleen@t-online.de>
2019-08-05 17:01:04 +0200

    Added covariates to DESeq

--- Plot MAST output against DESeq.R
+++ Plot MAST output against DESeq.R
@@ -1,13 +1,20 @@
 #Plotting q values from MAST output against padj from DESeq output
-
 res[ order(res$padj), ]
 results_lucas <- read.delim(file="~/Desktop/ASD/S4.csv", sep=";", dec=",")
 results_lucas <- results_lucas[results_lucas$X...Cell.type == "L2/3", ]
 res_L23 <- res[rownames(res) %in% results_lucas$Gene.name, ]
-results_joined <- left_join(as.tibble(results_lucas), 
-          as.tibble(rownames_to_column(as.data.frame(res_L23), var="Gene.name")), by="Gene.name")
-plot(log(results_joined$q.value), log(results_joined$padj), xlab="q.value from Schirmer", ylab="padj from DESeq")
 
+results1 <- left_join(as.tibble(results_lucas), 
+          as.tibble(rownames_to_column(as.data.frame(res_L23), var="Gene.name")), by="Gene.name")
+plot(log(results1$q.value), log(results1$padj), xlab="q.value from Schirmer", ylab="padj from DESeq")
+abline(0,1)
+
+res_HS6ST3 <- res2[rownames(res2) %in% results_lucas$Gene.name, ]
+
+results <- left_join(as_tibble(results_lucas), 
+                     as_tibble(res_HS6ST3, rownames="Gene.name"))
+plot(log(results$q.value), log(results$padj2), xlab="q.value from Schirmer", ylab="padj from DESeq clustered by HST...")
+abline(0,1)
 
 
 tibble(counts["HS6ST3",])
@@ -19,8 +26,8 @@ as_tibble(cellinfo)%>%
 res[ order(res$padj), ]
 res2[order(res$padj), ]
 colnames(res2) <- c("baseMean2", "log2FoldChange2", "lfcSE2", "stat2", "pvalue2", "padj2")
-results_joined <- left_join(as_tibble(rownames_to_column(as.data.frame(res2))), 
-                            as_tibble(rownames_to_column(as.data.frame(res))))
+results_joined <- left_join(as_tibble(res2, rownames="Gene.name"), 
+                            as_tibble(res, rownames="Gene.name"))
 plot(log10(results_joined$padj), log10(results_joined$padj2), xlab="Sorting by Paper", ylab="Sorting by HS6ST3")
 abline(0,1)
 
